1ab5b8d7f3f1bc3e325aab9149dc4d7785c738d9
danilogoncalves94 <danilogoncalvesdg21@gmail.com>
2023-03-15 17:38:24 -0300

    upgrades

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function funcMail() {
+    $('#email').keyup(function (e) {
+        let nome = $(this).val()
+        if (nome.length > 0) {
+            $('#email').css({fontFamily: 'sans-serif'})
+        } else {
+            $('#email').css({fontFamily: 'Glitch'})
+        }
+    })
+}
+
+function funcSenha() {
+    $('#senha').keyup(function (e) {
+        let nome = $(this).val()
+        if (nome.length > 0) {
+            $('#senha').css({fontFamily: 'sans-serif'})
+        } else {
+            $('#senha').css({fontFamily: 'Glitch'})
+        }
+    })
+}
